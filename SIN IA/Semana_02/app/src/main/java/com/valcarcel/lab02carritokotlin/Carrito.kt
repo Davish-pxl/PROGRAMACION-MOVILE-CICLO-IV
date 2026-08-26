be1ac1dec9f7c777bae88f24241098a1bab881c4
Davish <guettavalcarcel@gmail.com>
2026-08-26 18:58:07 -0500

    Agrega data class Producto y funciones de subtotal, IGV y total

--- SIN IA/Semana_02/app/src/main/java/com/valcarcel/lab02carritokotlin/Carrito.kt
+++ SIN IA/Semana_02/app/src/main/java/com/valcarcel/lab02carritokotlin/Carrito.kt
@@ -1,7 +1,50 @@
 package com.valcarcel.lab02carritokotlin
 
-fun main() {
+data class Producto(
+    val nombre: String,
+    val precio: Double,
+    var cantidad: Int
+)
+
+fun main(){
     println("=========================================")
-    println(" CARRITO DE COMPRAS - TIENDA TECSUP ")
+    println("      CARRITO DE COMPRAS - TIENDA TECSUP ")
     println("=========================================")
+    val nombreCliente = "David Valcarcel"
+    val carrito = mutableListOf<Producto>()
+    println("Cliente: $nombreCliente")
+    println()
+
+    carrito.add(Producto("Laptop HP",2500.0,1))
+    carrito.add(Producto("Mouse Logitech",45.5,2))
+    carrito.add(Producto("Audifonos Logitech",120.0,2))
+    carrito.add(Producto("USB Kingston 64GB",45.5,3))
+
+    for(producto in carrito){
+        println("Producto agregado: ${producto.nombre}")
+    }
+    println()
+
+    val subtotal = calcularSubtotal(carrito)
+    val igv = calcularIgv(subtotal)
+    val total = calcularTotal(subtotal, igv)
+
+    println("Subtotal   : S/ $subtotal")
+    println("IGV (18%)  : S/ $igv")
+    println("TOTAL      : S/ $total")
+}
+fun calcularSubtotal(productos: List<Producto>): Double {
+    var subtotal = 0.0
+    for (p in productos) {
+        subtotal += p.precio * p.cantidad
+    }
+    return subtotal
+}
+
+fun calcularIgv(subtotal: Double): Double {
+    return subtotal * 0.18
+}
+
+fun calcularTotal(subtotal: Double, igv: Double): Double {
+    return subtotal + igv
 }
