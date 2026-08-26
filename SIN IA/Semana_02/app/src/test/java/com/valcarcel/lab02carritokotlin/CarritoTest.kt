5d763fe96bea02dedd80415e4e023ef0ab634f1f
Davish <guettavalcarcel@gmail.com>
2026-08-26 12:53:19 -0500

    Proyecto inicial con menú del carrito

---
+++ SIN IA/Semana_02/app/src/test/java/com/valcarcel/lab02carritokotlin/CarritoTest.kt
@@ -0,0 +1,13 @@
+package com.valcarcel.lab02carritokotlin
+
+import org.junit.Test
+
+class CarritoTest {
+
+    @Test
+    fun ejecutarCarrito() {
+        println("=========================================")
+        println(" CARRITO DE COMPRAS - TIENDA TECSUP ")
+        println("=========================================")
+    }
+}
