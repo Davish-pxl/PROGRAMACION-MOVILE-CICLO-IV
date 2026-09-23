efd607a9f05077649452d4f28e251073fb134f27
Davish-pxl <guettavalcarcel@gmail.com>
2026-09-22 20:21:55 -0500

    implementacion de AppNavigation con NavController y NavHost con el registro de cada pantalla

--- CON IA/Semana_05/app/src/main/java/com/tuapp/clinicasalud/navigation/AppNavigation.kt
+++ CON IA/Semana_05/app/src/main/java/com/tuapp/clinicasalud/navigation/AppNavigation.kt
@@ -1,1 +1,36 @@
 package com.tuapp.clinicasalud.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.tuapp.clinicasalud.screens.*
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+        composable(Screen.Home.route) {
+            HomeScreen(navController)
+        }
+        composable(Screen.Profile.route) {
+            ProfileScreen(navController)
+        }
+        composable(Screen.Schedule.route) {
+            ScheduleScreen(navController)
+        }
+        composable(Screen.Success.route) {
+            SuccessScreen(navController)
+        }
+        composable(Screen.Appointments.route) {
+            AppointmentsScreen(navController)
+        }
+        composable(Screen.History.route) {
+            HistoryScreen(navController)
+        }
+    }
+}
--- CON IA/Semana_05/app/src/main/java/com/tuapp/clinicasalud/navigation/Screen.kt
+++ CON IA/Semana_05/app/src/main/java/com/tuapp/clinicasalud/navigation/Screen.kt
@@ -1,4 +1,15 @@
 package com.tuapp.clinicasalud.navigation
 
-class Screen {
+sealed class Screen (val route:String) {
+    object Home : Screen("home")
+
+    object Appointments : Screen("appointments")
+
+    object Profile : Screen( "profile")
+
+    object Schedule: Screen("schedule")
+
+    object Success: Screen("success")
+
+    object History: Screen("history")
 }
